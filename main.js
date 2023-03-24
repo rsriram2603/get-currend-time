b9298f5f47a3078df012ac1c9e962b452a8fe148
rsriram2603 <rsriram830@gmail.com>
2023-03-24 10:36:10 +0530

    new file add

---
+++ main.js
@@ -0,0 +1,14 @@
+let a=new Date();
+	let res=a.getTime();
+	if(res>=6 && res<=11)
+	{
+		console.log("mornimg");
+	}
+	else if(res>=12 && res<=16)
+	{
+		console.log("afternoon");
+	}
+	else
+	{
+	console.log("night");
+	}
